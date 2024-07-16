aee8c1fcf9dbc95d89cc4ee41f67d6b8a580ea82
CHIAYAFICANTONA <cchiayafi@gmail.com>
2024-07-16 19:09:36 +0100

    added james' pages

--- Bus_Booking_System-webEnd/js/app.js
+++ Bus_Booking_System-webEnd/js/app.js
@@ -7,3 +7,4 @@ async function includeHTML(elementId, file) {
 includeHTML('samuel','/pages/samuel_page.html');
 includeHTML('cantona','/pages/cantona_page.html');
 includeHTML('marcus','/pages/marcus_page.html');
+includeHTML('james','/pages/james_page.html');
