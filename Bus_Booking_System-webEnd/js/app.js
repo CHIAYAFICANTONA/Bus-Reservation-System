09bf506ca110c4be78c6eb2ec9fe4a738aada2a3
noubissie pavel <noubissiepavel@gmail.com>
2024-08-25 21:24:18 +0100

    [clean] - signIn - signUp

--- Bus_Booking_System-webEnd/js/app.js
+++
@@ -1,7 +0,0 @@
-async function includeHTML(elementId, file) {
-  const response = await fetch(file);
-  const htmlContent = await response.text();
-  document.getElementById(elementId).innerHTML = htmlContent;
-}
-
-IncludeHTML('sectionOne','/pages/sectionOne.html');
