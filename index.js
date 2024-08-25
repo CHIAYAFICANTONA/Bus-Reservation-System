9b6d70dd14590a955e6152f32424993426ebbee6
noubissie pavel <noubissiepavel@gmail.com>
2024-08-25 18:22:33 +0100

    [feat] - Init web project

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const concurrently = require('concurrently');
 
 const commands = [
-{ command: 'cd Bus_Booking_System-MobileEnd && npx ionic serve', name: 'Ionic', prefixColor: 'blue' },
-{ command: 'cd Bus_Booking_System-webEnd && npx serve -s .', name: 'HTML5', prefixColor: 'green' }
+// { command: 'cd Bus_Booking_System-MobileEnd && npx ionic serve', name: 'Ionic', prefixColor: 'blue' },
+{ command: 'cd Bus_Booking_System-webEnd && npx serve .', name: 'HTML5', prefixColor: 'green' }
 ];
 
 concurrently(commands, {
