32c61b4c1fd1712af10df529cca8781872fe6f51
Samuel-Anjoh-Jr <anjohsamueljr@gmail.com>
2024-09-01 13:10:42 +0100

    Responsive Home Page

--- Bus_Booking_System/src/app/shared/components/header/header.component.ts
+++ Bus_Booking_System/src/app/shared/components/header/header.component.ts
@@ -14,23 +14,28 @@ export class HeaderComponent {
   public headerItems: HeaderItems[] = [
     {
       id: 1,
-      link: '',
+      link: '#home',
       title: 'Home'
     },
     {
       id: 2,
-      link: '',
-      title: 'Bookings'
+      link: '#offers',
+      title: 'Offers'
     },
     {
       id: 3,
-      link: '',
-      title: 'About'
+      link: '#howTo',
+      title: 'How To'
     },
     {
       id: 4,
-      link: '',
-      title: 'Contact'
+      link: '#popular',
+      title: 'Popular Destinations'
+    },
+    {
+      id: 5,
+      link: '#faq',
+      title: 'FAQs'
     }
   ];
 
