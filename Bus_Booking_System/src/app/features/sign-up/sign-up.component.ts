69183a4aa66c8632d6d25a57a96f7d88da126702
Samuel-Anjoh-Jr <anjohsamueljr@gmail.com>
2024-09-02 19:45:36 +0100

    SignUp LogIn

--- Bus_Booking_System/src/app/features/sign-up/sign-up.component.ts
+++ Bus_Booking_System/src/app/features/sign-up/sign-up.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    RouterLink
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
