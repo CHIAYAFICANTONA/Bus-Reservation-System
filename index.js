9c9bac0b6ec19d1883f9e73a1078537c62a25585
Samuel-Anjoh-Jr <anjohsamueljr@gmail.com>
2024-09-06 10:35:07 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const concurrently = require('concurrently');
 
 const commands = [
 // { command: 'cd Bus_Booking_System-MobileEnd && npx ionic serve', name: 'Ionic', prefixColor: 'blue' },
-{ command: 'cd Bus_Booking_System-webEnd && npx serve .', name: 'HTML5', prefixColor: 'green' }
+{ command: 'cd Bus_Booking_System && ng serve',
+    name: 'Angular',
+    prefixColor: 'green' },
+    { command: 'cd Bus_Booking_System && cd Bus_Booking_System-BackEnd && npm run reload',
+        name: 'EndPoints',
+        prefixColor: 'blue' }
 ];
 
 concurrently(commands, {
