8cdcafa8f7dd662a25fcb59de7f226312509ddfa
Samuel-Anjoh-Jr <anjohsamueljr@gmail.com>
2024-09-06 11:41:55 +0100

    Updated seats and linked pages

--- Bus_Booking_System/src/app/features/seats/seats.component.ts
+++ Bus_Booking_System/src/app/features/seats/seats.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-seats',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './seats.component.html',
   styleUrl: './seats.component.css'
 })
